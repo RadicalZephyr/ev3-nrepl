22114e1caee98c348c07c868c0131bae047bb8da
Geoff Shannon <geoffpshannon@gmail.com>
2013-12-11 22:45:23 -0800

    Update aliases for greater efficacy

--- project.clj
+++ project.clj
@@ -5,7 +5,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :uberjar-name "ev3nrepl.jar"
   :main ev3nrepl.core
-  :aliases {"pushev3" ["shell" "scp" "target/ev3nrepl.jar" "root@10.0.1.1:"]}
+  :aliases {"run-ev3" ["do" "uberjar,"
+                       "shell" "scp" "target/ev3nrepl.jar" "root@10.0.1.1:,"
+                       "shell" "ssh" "-f" "root@10.0.1.1" "/bin/jrun -jar /home/root/ev3nrepl.jar"]
+            "shutdown-ev3" ["shell" "ssh" "root@10.0.1.1" "halt"]}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.nrepl "0.2.3"]
                  [org.clojars.earthlingzephyr/lejos-ev3 "0.5.0-SNAPSHOT"]]
